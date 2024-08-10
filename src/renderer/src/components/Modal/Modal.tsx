a8e28b22ea0a98e06ad8517673003c1d0668711a
Yaroslav <yarik.pavlov.971@gmail.com>
2024-08-10 16:38:37 +0300

    - Improved modal animation - Changed grid transition

--- src/renderer/src/components/Modal/Modal.tsx
+++ src/renderer/src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import Close from '@renderer/icons/Close';
 import cn from 'classnames';
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import SwitchTransition from '../SwitchTransition';
 import styles from './Modal.module.scss';
@@ -12,7 +12,37 @@ interface Props {
   className?: string;
 }
 
+const posMultiplier = 0.3;
+
 const Modal = ({ open, onClose, children, className }: Props) => {
+  const [initialPos, setInitialPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [cursorPos, setCursorPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const modal = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      return;
+    }
+
+    const handleMouseMove = (event: MouseEvent) => {
+      setCursorPos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!open) {
+      return;
+    }
+
+    setInitialPos({ x: cursorPos.x - window.innerWidth / 2, y: cursorPos.y - window.innerHeight / 2 });
+  }, [open]);
+
   return createPortal(
     <div className={styles.container}>
       <SwitchTransition
@@ -30,9 +60,21 @@ const Modal = ({ open, onClose, children, className }: Props) => {
           exit: styles.exit,
         }}
         timeout={200}
+        nodeRef={modal}
       >
         {open && (
-          <div className={cn(styles.modal, className)} onMouseDown={(e) => e.stopPropagation()}>
+          <div
+            ref={modal}
+            className={cn(styles.modal, className)}
+            onMouseDown={(e) => e.stopPropagation()}
+            style={
+              {
+                '--initial-translate': `translate(${initialPos.x * posMultiplier}px, ${
+                  initialPos.y * posMultiplier
+                }px)`,
+              } as CSSProperties
+            }
+          >
             {onClose && (
               <button onClick={onClose} className={styles.closeButton}>
                 <Close />
--- src/renderer/src/pages/Home/Home.tsx
+++ src/renderer/src/pages/Home/Home.tsx
@@ -1,32 +1,46 @@
 import EmptyPlaceholder from '@renderer/components/EmptyPlaceholder';
+import LayoutTransition from '@renderer/components/LayoutTransition';
 import Track from '@renderer/components/Track';
-import useViewTransitionState from '@renderer/hooks/useViewTransitionState';
+import useDelayedState from '@renderer/hooks/useDelayedState';
 import SoundCloud from '@renderer/icons/SoundCloud';
 import YouTube from '@renderer/icons/YouTube';
 import usePlayerStore from '@renderer/store/playerStore';
 import styles from './Home.module.scss';
+import SwitchTransition from '@renderer/components/SwitchTransition';
 
 const Home = () => {
   const originalTracks = usePlayerStore((st) => st.tracks);
-  const [tracks, isTransitioning] = useViewTransitionState(originalTracks);
+  const [tracks] = useDelayedState(originalTracks, true);
 
   return (
     <ul className={styles.grid}>
-      {!tracks.length && (
-        <EmptyPlaceholder
-          title="Your playlist appears to be empty"
-          caption={
-            <>
-              Use <YouTube className={styles.icon} /> or <SoundCloud className={styles.icon} /> to find and add tracks
-              to the playlist
-            </>
-          }
-        />
-      )}
-      {tracks.map((i) => (
-        <li style={{ viewTransitionName: isTransitioning ? `track-${i.id}` : 'none' }} key={i.id}>
-          <Track track={i} />
-        </li>
+      <SwitchTransition
+        classes={{
+          enter: styles.enter,
+          exit: styles.exit,
+        }}
+        timeout={200}
+      >
+        {!tracks.delayed.length && (
+          <div className={styles.placeholderTransition}>
+            <EmptyPlaceholder
+              title="Your playlist appears to be empty"
+              caption={
+                <>
+                  Use <YouTube className={styles.icon} /> or <SoundCloud className={styles.icon} /> to find and add
+                  tracks to the playlist
+                </>
+              }
+            />
+          </div>
+        )}
+      </SwitchTransition>
+      {tracks.delayed.map((i) => (
+        <LayoutTransition key={i.id} deps={[tracks.relevant]} delayedDeps={[tracks.delayed]} duration={200}>
+          <li>
+            <Track track={i} />
+          </li>
+        </LayoutTransition>
       ))}
     </ul>
   );
